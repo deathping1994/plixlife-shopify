69d045bfc693dd991d02727d3dccf4222bf7e8f5
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-08-04 13:50:00 +0530

    Update prepaidoffer.js

--- prepaidoffer.js
+++ prepaidoffer.js
@@ -6,13 +6,12 @@ $(document).on(`page:load page:change`, function(event) {
         text-align: center;
         border-radius: 4px;
     ">
-
           <p class="section__text" style="
         margin: 0px;
     ">Avail Rs. 100 off your order with code PREPAID by paying online now  via Razorpay/Paytm/UPI</p>
         </div>`
     $(".section--payment-method").prepend(prepaid_offer_html)
-    if($('input[name="checkout[payment_gateway]"]:checked').val() == "64480837791" && $(".reduction-code__text:first").text()=="PREPAIDTEST" )}
+    if($('input[name="checkout[payment_gateway]"]:checked').val() == "64480837791" && $(".reduction-code__text:first").text()=="PREPAIDTEST" ){
         $(".tags-list .tag:first .tag__button").click()
     }
 });
