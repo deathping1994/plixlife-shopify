77777d74361f0bfb389af2844e019accee6a085f
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-08-04 13:21:53 +0530

    Create prepaidoffer.js

---
+++ prepaidoffer.js
@@ -0,0 +1,27 @@
+$(document).on(`page:load page:change`, function(event) {
+    var prepaid_offer_html = `<div class="section__header" style="
+        background-color: #03a196;
+        padding: 12px 8px;
+        color: #ffffff;
+        text-align: center;
+        border-radius: 4px;
+    ">
+
+          <p class="section__text" style="
+        margin: 0px;
+    ">Avail Rs. 100 off your order with code PREPAID by paying online now  via Razorpay/Paytm/UPI</p>
+        </div>`
+    $(".section--payment-method").prepend(prepaid_offer_html)
+    if($('input[name="checkout[payment_gateway]"]:checked').val() != "64480837791"){
+        $("#checkout_reduction_code_mobile")[0].value = "PREPAIDTEST";
+        $("#checkout_reduction_code_mobile").parent().next().removeAttr("disabled").click();
+    }
+    $('.section--payment-method input').on('change', function() {
+    if($('input[name="checkout[payment_gateway]"]:checked').val() == "64480837791"){
+        if($(".reduction-code__text:first").text()=="PREPAIDTEST"){
+            $(".tags-list .tag:first .tag__button").click()
+        }
+    }
+    });
+});
+
